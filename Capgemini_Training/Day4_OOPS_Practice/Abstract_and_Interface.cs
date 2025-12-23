4d8fa3c6d36e8b0ef58e7720b5b36416cf5f2127
Shreyansh jain <118683295+shreyjain2003@users.noreply.github.com>
2025-12-23 23:49:31 +0530

    Proper Formatting

---
+++ Capgemini_Training/Day4_OOPS_Practice/Abstract_and_Interface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capgemini_Training.Oops
+{
+    interface ILoggable { void Log(string message); }
+    interface INotifiable { void Notify(string message); }
+
+    abstract class ServiceBase
+    {
+        protected string ServiceName { get; }
+        protected ServiceBase(string serviceName) => ServiceName = serviceName;
+
+        public void Start() => Console.WriteLine($"{ServiceName} started.");
+        public abstract void Execute();
+    }
+
+    class EmailService : ServiceBase, ILoggable, INotifiable
+    {
+        public EmailService() : base("EmailService") { }
+
+        public override void Execute()
+        {
+            Log("Executing email pipeline...");
+            Notify("Email batch started");
+            Console.WriteLine("Sending emails...");
+        }
+
+        public void Log(string message) => Console.WriteLine($"[LOG] {message}");
+        public void Notify(string message) => Console.WriteLine($"[NOTIFY] {message}");
+    }
+}
+
+// The following code should be inside a Main method in a Program class
+class Abstract_and_Interface
+{
+    public static void Main()
+    {
+        var svc = new Capgemini_Training.Oops.EmailService();
+        svc.Start();
+        svc.Execute();
+    }
+}
